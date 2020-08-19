c67d6f21848a7cc0caca636fb3b589ea747de559
ayushmawasthi <ayushmawasthi@gmail.com>
2020-08-20 04:45:03 +0530

    user home day 1

--- IndoorVicinity/app/src/main/java/com/tcs/indoorvicinity/UserHome.java
+++ IndoorVicinity/app/src/main/java/com/tcs/indoorvicinity/UserHome.java
@@ -1,15 +1,34 @@
 package com.tcs.indoorvicinity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class UserHome extends AppCompatActivity {
     LinearLayout l1,l2;
+    RecyclerView recyclerView,recyclerView2;
+    RecyclerView.Adapter myAdapter,myAdapter2;
+    RecyclerView.LayoutManager layoutManager,layoutManager2;
+    ArrayList<Products> product;
+    ImageView productview,searchbutton;
+    AutoCompleteTextView etsearch;
+    String []names={"Raymond","Shirts","TShirts","Glasses","Woodland","Reebok","Adidas","Peter England","Intex"};
+    TextView t1temp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +36,46 @@ public class UserHome extends AppCompatActivity {
         setContentView(R.layout.activity_user_home);
         l1=findViewById(R.id.llproduct);
         l2=findViewById(R.id.llshop);
+        etsearch =(AutoCompleteTextView) findViewById(R.id.userhomesearchbar);
+        recyclerView=findViewById(R.id.userhomeproduct1);
+        recyclerView2=findViewById(R.id.userhomeshop1);
+        searchbutton=findViewById(R.id.userhomesearchbutton);
+        t1temp=findViewById(R.id.temp1);
+        t1temp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(UserHome.this,com.tcs.indoorvicinity.Customer_View_Item.class);
+                startActivity(i);
+            }
+        });
+//        recyclerView.setHasFixedSize(true);
+
+        layoutManager =new GridLayoutManager(this,2,GridLayoutManager.HORIZONTAL,false);
+        layoutManager2 =new GridLayoutManager(this,2,GridLayoutManager.HORIZONTAL,false);
+
+//        layoutManager =new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView2.setLayoutManager(layoutManager2);
+        product=new ArrayList<Products>();
+        product.add(new Products("T Shirt","Raymond","50"));
+        product.add(new Products("Shirt","LeeCooper","70"));
+        product.add(new Products("Jeans","Lewis","50"));
+        product.add(new Products("Socks","Puma","50"));
+        product.add(new Products("Shoes","Reebok","20"));
+        product.add(new Products("Tie","Raymond","60"));
+        myAdapter=new UserHomeAdapter1(this,product);
+        recyclerView.setAdapter(myAdapter);
+        myAdapter.notifyDataSetChanged();
+        myAdapter2=new UserHomeAdapter2(this,product);
+        recyclerView2.setAdapter(myAdapter2);
+        myAdapter2.notifyDataSetChanged();
+
+
+
+
+
+
         l1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,6 +92,21 @@ public class UserHome extends AppCompatActivity {
 
             }
         });
+        searchbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(etsearch.getVisibility()!=View.VISIBLE) {
+                    etsearch.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    etsearch.setVisibility(View.GONE);
+                }
+            }
+        });
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,names);
+        etsearch.setThreshold(1);
+        etsearch.setAdapter(arrayAdapter);
 
     }
 
---
+++ IndoorVicinity/app/src/main/java/com/tcs/indoorvicinity/UserHomeAdapter2.java
@@ -0,0 +1,73 @@
+package com.tcs.indoorvicinity;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+class UserHomeAdapter2 extends RecyclerView.Adapter<UserHomeAdapter2.ViewHolder> {
+    private ArrayList<Products> product;
+
+
+
+    public UserHomeAdapter2(Context context, ArrayList<Products> list) {
+        product=list;
+
+    }
+
+
+    class ViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView tv1;
+        ImageView itempic;
+        public ViewHolder(@NonNull final View itemView) {
+            super(itemView);
+            tv1=itemView.findViewById(R.id.userhometext1);
+
+            itempic=itemView.findViewById(R.id.userhomeimg1);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    System.out.println("Item clicked " +getItemCount() );
+                    System.out.println("2 "+getAbsoluteAdapterPosition());
+
+
+                }
+            });
+        }
+
+    }
+
+
+    @NonNull
+    @Override
+    public UserHomeAdapter2.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.userhomeadapterlist1,parent,false);
+        return new UserHomeAdapter2.ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserHomeAdapter2.ViewHolder holder, int position) {
+        holder.itemView.setTag(product.get(position));
+        holder.tv1.setText(product.get(position).getProduct_brand());
+        holder.itempic.setImageResource(R.drawable.car1);
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return product.size();
+
+    }
+}
